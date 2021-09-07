414a23ef58fe13798c3eb44deabe8b43ad7a1492
Your Name <you@example.com>
2021-09-07 20:19:38 +0200

    add/controller/improvement

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: %i[ show edit update destroy ]
+  before_action :current_user, only: [ :show, :edit, :update, :destroy ]
 
   # GET /users or /users.json
   def index
@@ -8,34 +8,12 @@ class UsersController < ApplicationController
 
   # GET /users/1 or /users/1.json
   def show
-  end
-
-  # GET /users/new
-  def new
-    @user = User.new
-  end
-
-  # GET /users/1/edit
-  def edit
-  end
-
-  # POST /users or /users.json
-  def create
-    @user = User.new(user_params)
-
-    respond_to do |format|
-      if @user.save
-        format.html { redirect_to @user, notice: "User was successfully created." }
-        format.json { render :show, status: :created, location: @user }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
-    end
+    @user = User.find(params[:id])
   end
 
   # PATCH/PUT /users/1 or /users/1.json
   def update
+    user = User.find(params[:id])
     respond_to do |format|
       if @user.update(user_params)
         format.html { redirect_to @user, notice: "User was successfully updated." }
@@ -45,25 +23,25 @@ class UsersController < ApplicationController
         format.json { render json: @user.errors, status: :unprocessable_entity }
       end
     end
+    redirect_to user
   end
 
   # DELETE /users/1 or /users/1.json
   def destroy
-    @user.destroy
+    user = User.find(params[:id])
+    user.destroy
     respond_to do |format|
       format.html { redirect_to users_url, notice: "User was successfully destroyed." }
       format.json { head :no_content }
     end
+    redirect_to root_path
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_user
-      @user = User.find(params[:id])
-    end
-
+   
     # Only allow a list of trusted parameters through.
     def user_params
-      params.require(:user).permit(:email)
-    end
+    params.require(:user).permit(:email, :password)
+  
+  end
 end
