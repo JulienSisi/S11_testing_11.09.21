154f0b910398ff0478c86a6feb77b05a6df7b6ef
tarekzemmouri <zemmouri.tarek@hotmail.com>
2021-09-09 10:50:25 +0200

    Action mailer et Module events, validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+        
+  def welcome_send
+      ContactMailer.welcome(self).deliver_now
+  end
+  
 end
