20af89ce4c58a745be358a1078fb65b1485a0ed0
stephanietako <gabeshine@live.fr>
2021-09-10 00:14:00 +0200

    add config/routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :admins
   devise_for :users
+  devise_scope :user do     get '/users/sign_out' => 'devise/sessions#destroy'
   root to: "static_pages#landing_page"
   resources :users
+  end
 end
