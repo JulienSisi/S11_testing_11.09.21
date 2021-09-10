83d2db7ddcb02bbb434263335e1f0b89e16023df
John78468 <philantrope1@outlook.com>
2021-09-10 20:33:43 +0200

    Version2 partner and dash(probleme avec configuration de devises registration

--- app/controllers/partners_office/partners_controller.rb
+++ app/controllers/partners_office/partners_controller.rb
@@ -1,7 +1,10 @@
 module PartnersOffice
   class PartnersController < ApplicationController
+
     def index
       @partners = Partner.all
+      @partners = current_partner
     end
+    
   end
 end
